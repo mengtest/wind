a112074485d3a51ba54fb126dd480d9788142899
windy <707298413@qq.com>
2019-12-20 00:43:27 +0800

    clear

--- service/main.lua
+++ service/main.lua
@@ -1,20 +1,18 @@
 local skynet = require "skynet"
 require "skynet.manager"
 local snax = require "skynet.snax"
-local server_conf = require "config.server"
+local server = require "config.server"
 
 skynet.start(function()
 
 	math.randomseed(tonumber(tostring(os.time()):reverse()))
 	skynet.error("=============================================")
-	skynet.error(os.date("%Y/%m/%d %H:%M:%S ")..server_conf.name.." start")
+	skynet.error(os.date("%Y/%m/%d %H:%M:%S ")..server.name.." start")
 	skynet.error("=============================================")
 
 	if not skynet.getenv "daemon" then
 		local console = skynet.newservice("console")
 	end
-
-
 	skynet.newservice("debug_console", 9999)
 
 	
